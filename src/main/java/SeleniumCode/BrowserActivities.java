611ad043b99d13f76bbbc9e6bfa7ed8a1e836c1c
unknown <shalini.onteddu@gmail.com>
2022-09-20 17:18:02 -0700

    removed todo comment

--- src/main/java/SeleniumCode/BrowserActivities.java
+++ src/main/java/SeleniumCode/BrowserActivities.java
@@ -8,7 +8,6 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class BrowserActivities {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
 		System.setProperty("webdriver.chrome.driver",
 				"C:\\Users\\Shalini\\chromedriver_win32\\chromedriver.exe");
